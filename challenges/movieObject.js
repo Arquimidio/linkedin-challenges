1ae06a6b97ddebb50247cce80f6a79c13e0cce51
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-01 20:11:52 -0300

    Add challenge instructions

---
+++ challenges/movieObject.js
@@ -0,0 +1,10 @@
+/*
+    1. It should have:
+        - title
+        - director
+        - genre
+        - release year
+        - rating
+    2. getOverview() = "<movie>, a <genre> film directed by <director> was released in <releaseYear>. It received a rating of <rating>"
+    3. Develop your solution through a constructor function (basic prototype, no class syntax)
+*/
