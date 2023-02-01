f0f2a06c45f2d622a7965663dedb1534e2398bd4
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-01 19:14:13 -0300

    Add availableBooks description

---
+++ challenges/availableBooks.js
@@ -0,0 +1,11 @@
+/*
+    1. Create a Book class with class syntax
+    2. It should have the properties: title, author, ISBN, numCopies
+    3. It should also have: 
+        - getAvailability(), being a getter
+            . If 0 copies return "out of stock"
+            . If less than 10 copies return "low stock"
+            . Else return "in stock"
+        - sell function, with default selling number to 1
+        - restock, with default restock value of 5
+*/
