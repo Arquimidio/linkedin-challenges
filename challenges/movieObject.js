74380cf003272a6c7c35587a397433e180be665c
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-01 20:41:41 -0300

    Add basic properties to movie object

--- challenges/movieObject.js
+++ challenges/movieObject.js
@@ -1,5 +1,5 @@
 /*
-    1. It should have:
+    1. The Movie object should have:
         - title
         - director
         - genre
@@ -8,3 +8,39 @@
     2. getOverview() = "<movie>, a <genre> film directed by <director> was released in <releaseYear>. It received a rating of <rating>"
     3. Develop your solution through a constructor function (basic prototype, no class syntax)
 */
+
+module.exports = function Movie(title, director, genre, releaseYear, rating) {
+    this._title = title;
+    this._director = director;
+    this._genre = genre;
+    this._releaseYear = releaseYear;
+    this._rating = rating;
+
+    Object.defineProperties(this, {
+        title: {
+            get() {
+                return this._title;
+            }
+        },
+        director: {
+            get() {
+                return this._director;
+            }
+        },
+        genre: {
+            get() {
+                return this._genre;
+            }
+        },
+        releaseYear: {
+            get() {
+                return this.releaseYear;
+            }
+        },
+        rating: {
+            get() {
+                return this._rating;
+            }
+        }
+    })
+}
