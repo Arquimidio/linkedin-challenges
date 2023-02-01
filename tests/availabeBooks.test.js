b179c7c7a9db9be7b087dc3c8132db3134dea36e
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-01 19:47:03 -0300

    Create new test suite and create new kinds of books

--- tests/availabeBooks.test.js
+++ tests/availabeBooks.test.js
@@ -1,18 +1,26 @@
 const Book = require('../challenges/availableBooks');
-let book;
+let outOfStockBook, lowStockBook, inStockBook;
 
 beforeEach(() => {
-    book = new Book('Lord Of The Programmers', 'Neo Protagonist', '424242', 13);
+    const bookData = ['Lord Of The Programmers', 'Neo Protagonist', '424242'];
+    outOfStockBook = new Book(...bookData, 13);
+    lowStockBook = new Book(...bookData, 5);
+    inStockBook = new Book(...bookData, 13);
 })
 
 describe("Property / Method is present" , () => {
     test("Constructor properties", () => {
-        expect(['title', 'author', 'ISBN', 'numCopies'].every(prop => prop in book)).toBe(true);
+        expect(['title', 'author', 'ISBN', 'numCopies'].every(prop => prop in outOfStockBook)).toBe(true);
     })
 
     test("Methods are present", () => {
-        expect(['availability', 'sell', 'restock'].every(method => method in book));
+        expect(['availability', 'sell', 'restock'].every(method => method in outOfStockBook)).toBe(true);
     })
 })
 
-describe("")
+describe("Method works as expected", () => {
+    
+    test("Availability returns the correct message", () => {
+        expect()
+    })
+})
