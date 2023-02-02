f52022e2f459b3449580c77f629d7c1b85354283
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-02 10:10:31 -0300

    Create User class

--- challenges/privateObject.js
+++ challenges/privateObject.js
@@ -3,3 +3,24 @@
     2. Using symbols, ensure that username and password are private fields on the user character
     (they shouldn't be accessbile by calling user.username or user.password)
 */
+
+const usernameSymbol = Symbol('user');
+const passwordSymbol = Symbol('pswd');
+
+class User {
+    constructor(username, password, age ) {
+        this[usernameSymbol] = username;
+        this[passwordSymbol] = password;
+        this._age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+}
+
+module.exports = {
+    User,
+    usernameSymbol,
+    passwordSymbol
+}
