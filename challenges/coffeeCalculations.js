05c6063ea633d9880aadda038b33365d9b90280a
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-02 16:46:14 -0300

    Create CoffeCalculations class

--- challenges/coffeeCalculations.js
+++ challenges/coffeeCalculations.js
@@ -4,3 +4,23 @@
   3. You should return the result in the format: "The total bill is Total"
   4. You should use template literals for your return value
 */
+
+module.exports = class CoffeeCalculations {
+  constructor(coffeeOrders = [], pricePerCoffee = 1) {
+    this._coffeeOrders = coffeeOrders;
+    this._pricePerCoffee = pricePerCoffee;
+  }
+
+  get coffeeOrders() {
+    return this._coffeeOrders;
+  }
+
+  get pricePerCoffee() {
+    return this._pricePerCoffee;
+  }
+
+  get total() {
+    return this.coffeeOrders
+      .reduce((acc, cur) => acc + cur * this.pricePerCoffee, 0)
+  }
+}
