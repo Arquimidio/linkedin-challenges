a969765023fec81c13a67f615d3a81a329669f6b
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-02 16:32:03 -0300

    Add description

---
+++ challenges/coffeeCalculations.js
@@ -0,0 +1,6 @@
+/*
+  1. Use reduce to calculate the total bill
+  2. Given an array of coffee orders and the price per coffee, do the calculations
+  3. You should return the result in the format: "The total bill is Total"
+  4. You should use template literals for your return value
+*/
