d9d45df9d4bf919e3b4c0cd8bb9ef451e0daa96a
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-01 21:15:30 -0300

    Add the only necessary test for now

--- tests/vegetarianDinner.test.js
+++ tests/vegetarianDinner.test.js
@@ -1,1 +1,6 @@
 const vegetarianDishes = require('../challenges/vegetarianDinner');
+const dummyDishes = require('../dummy-data/dishes.json');
+
+test("Returns vegetarian dishes only", () => {
+    expect(vegetarianDishes(dummyDishes).every(dish => dish.vegetarian)).toBe(true);
+})
