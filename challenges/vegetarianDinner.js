ff28f7dd12b19a2d2c041025f03a8fc3204e913c
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-01 21:12:26 -0300

    Finish dummy data creation

--- challenges/vegetarianDinner.js
+++ challenges/vegetarianDinner.js
@@ -1,4 +1,8 @@
 /*
     1. Using filter, return only the dishes that are vegetarian
+    2. I'm not doing the DOM part because this is Node, so no DOM
 */
 
+module.exports = function vegetarianDishes(array) {
+    return array.filter(dish => dish.vegetarian);
+}
---
+++ tests/vegetarianDinner.test.js
@@ -0,0 +1,1 @@
+const vegetarianDishes = require('../challenges/vegetarianDinner');
