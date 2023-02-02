efa1956cb7a58edd829127ff1af71830c4c2c054
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-02 08:02:11 -0300

    Add description

---
+++ challenges/privateObject.js
@@ -0,0 +1,5 @@
+/*
+    1. The User object should have: username, password, age
+    2. Using symbols, ensure that username and password are private fields on the user character
+    (they shouldn't be accessbile by calling user.username or user.password)
+*/
