4ef9f776e6aa5068adc29ca6935d29d1a03fe86f
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-01 21:05:31 -0300

    Add instructions and dummy data

---
+++ challenges/vegetarianDinner.js
@@ -0,0 +1,18 @@
+/*
+    1. Using filter, return only the dishes that are vegetarian
+*/
+
+const dishes = [
+    {
+        name: "Meatn't",
+        vegetarian: true
+    },
+    {
+        name: "Cheesen't",
+        vegetarian: true
+    },
+    {
+        name: "Milkn't",
+        vegetarian: true
+    }
+]
