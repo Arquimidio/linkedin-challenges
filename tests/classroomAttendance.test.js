f874bfd6ce9b81d2041e1bcfc159ec7059bdcaf0
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-02 13:38:47 -0300

    Add tests for Classroom

--- tests/classroomAttendance.test.js
+++ tests/classroomAttendance.test.js
@@ -1,8 +1,37 @@
-const { beforeEach } = require('node:test');
+const { beforeEach, default: test, describe } = require('node:test');
 const Classroom = require('../challenges/classroomAttendance');
 let classroomWithAssistant;
 let classroomWithoutAssistant;
+let people;
 
 beforeEach(() => {
-  classroomWithAssistant = new Classroom();
+  people = ['Jorge', 'Frank', 'Steve', 'Elon', 'Jeff', 'Tim'];
+  classroomWithAssistant = new Classroom(people, true);
+  classroomWithoutAssistant = new Classroom(people, false);
+})
+
+describe("Exists", () => {
+  test("Properties", () => {
+    expect(
+      ['people', 'hasTeachingAssistant'].every(prop => prop in classroomWithAssistant)
+    ).toBe(true);
+  })
+
+  test("Methods", () => {
+    [students].every(method => method in classroomWithAssistant)
+  })
+})
+
+describe("Works as expected", () => {
+  test("Classroom with assistant returns correct students", () => {
+    expect(
+      classroomWithAssistant.students
+    ).toEqual(people.slice(2))
+  })
+
+  test("Classroom without assistant returns correct students", () => {
+    expect(
+      classroomWithoutAssistant.students
+    ).toEqual(people.slice(1))
+  })
 })
