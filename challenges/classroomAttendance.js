f775feb21b7be66cd7122fb275e80a6a80f502be
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-02 13:17:05 -0300

    Add instructions

---
+++ challenges/classroomAttendance.js
@@ -0,0 +1,9 @@
+/*
+  1. Create a method called getStudents that will receive a Classroom class
+  2. Class will have a boolean called hasTeachingAssistant and an array of people
+  3. The first value in the array of people will be the teacher, and
+   if the classRoom has a teaching assistant, the second value will be the
+   teaching assistant
+  4. The rest will be the students
+  5. getStudents should return only the students
+*/
