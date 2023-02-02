d7a6c807b65aec89c40e884f60e0e09d98854e0f
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-02 17:31:10 -0300

    Add test for masterMenu

--- tests/foodTruck.test.js
+++ tests/foodTruck.test.js
@@ -1,1 +1,11 @@
-const MasterMenu = require('../challenges/foodTruck');
+const masterMenu = require("../challenges/foodTruck");
+const dummyMenus = require("../dummy-data/menus.json");
+
+describe("Works as expected", () => {
+  test("Removes duplicates", () => {
+    expect(
+      masterMenu(dummyMenus)
+        .every((item, i, arr) => arr.indexOf(item, i) === -1)
+    ).toBe(true)
+  })
+})
