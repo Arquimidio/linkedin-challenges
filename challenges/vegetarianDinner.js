b785291bbef107d4ed4516ab694c383d29769a56
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-01 21:06:40 -0300

    Move dishes to dummy-data and turn it into json

--- challenges/vegetarianDinner.js
+++ challenges/vegetarianDinner.js
@@ -2,17 +2,3 @@
     1. Using filter, return only the dishes that are vegetarian
 */
 
-const dishes = [
-    {
-        name: "Meatn't",
-        vegetarian: true
-    },
-    {
-        name: "Cheesen't",
-        vegetarian: true
-    },
-    {
-        name: "Milkn't",
-        vegetarian: true
-    }
-]
